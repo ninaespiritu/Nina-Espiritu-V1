fb364ddf7ad05f3d3262d983711f1739e4269b98
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-05 20:30:10 +0000

    Add Header component, add basic CSS

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header>
+            <h3>Hello! I am</h3>
+            <h1>Nina Espiritu</h1>
+            <p>Software Developer &amp; Designer</p>
+            <a href="#">
+                <button>About Me</button>
+            </a>
+        </header>
+    )
+};
+
+export default Header;
