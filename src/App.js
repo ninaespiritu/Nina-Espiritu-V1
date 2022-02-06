320771568c5688c2fe6ab7c2c2e4da92cc0bc436
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-06 23:44:10 +0000

    Add Socials component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Socials from './components/Socials';
 
 function App() {
   return (
     <div className="app">
       <div className="app-container">
+        <Socials />
         <Header />
         <AboutMe />
         <Portfolio />
