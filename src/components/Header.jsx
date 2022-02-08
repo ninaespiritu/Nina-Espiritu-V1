c4fb297d37e155adaea487bec5406e9aef925f4e
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-08 17:22:05 +0000

    Add header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./styles/Header.css";
+import FullArt from "./../images/home-fullart.png";
 
 const Header = () => {
     return (
         <header id="Home">
-            <h3>Hello! I am</h3>
-            <h1>Nina Espiritu</h1>
-            <p>Software Developer &amp; Designer</p>
-            <a href="#About">
-                <button>About Me</button>
-            </a>
+            <div className="header-text">
+                <h3>Hello! I am</h3>
+                <h1>Nina Espiritu</h1>
+                <h2>Software Developer &amp; Designer</h2>
+                <a href="#About">
+                    <button>About Me</button>
+                </a>
+            </div>
+            
+            <div className="art">
+                <div className="art-full">
+                    <img src={FullArt} alt="#" />
+                </div>
+            </div>
+            
         </header>
     )
 };
