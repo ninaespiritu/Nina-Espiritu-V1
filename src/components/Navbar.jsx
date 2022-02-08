a60d7232678fe214f1d0c293d2419d439db6f2d1
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-08 12:06:21 +0000

    Update navbar with responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
     return (
-        <nav>
-            <ul>
-                <a href="#Home">
-                    <li>Home</li>
-                </a>
-                <a href="#About">
-                    <li>About</li>
-                </a>
-                <a href="#Portfolio">
-                    <li>Portfolio</li>
-                </a>
-                <a href="#Contact">
-                    <li>Contact</li>
-                </a>
+        <nav className='navbar'>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'><a href='#Home'>Home</a></li>
+                <li className='nav-item'><a href='#About'>About</a></li>
+                <li className='nav-item'><a href='#Portfolio'>Portfolio</a></li>
+                <li className='nav-item'><a href='#Contact'>Contact</a></li>
             </ul>
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes size={24} style={{ color: '#a8a5b6' }} />) : (<FaBars size={24} style={{ color: '#a8a5b6' }} />)}
+
+            </div>
         </nav>
     )
 };
