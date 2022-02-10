2c7856dbc7cdb42abe62c4091ecaca35279f7514
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-02-10 21:17:18 +0000

    Add Footer component, move Socials to Footer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Socials from './components/Socials';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
       <div className="app-container">
         <Navbar />
-        <Socials />
         <Header />
         <AboutMe />
         <Portfolio />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles/Footer.css";
+import Socials from "./Socials";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Socials />
+            <p>Built and designed by Nina Espiritu</p>
+            <p>&#169; 2022 All rights reserved</p>
+        </footer>
+    )
+};
+
+export default Footer;
